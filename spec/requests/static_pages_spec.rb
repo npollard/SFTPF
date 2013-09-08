f055a1136c145dd90ef428d7f895ec524007b653
nelson pollard <nelson.pollard@sv.cmu.edu>
2013-09-08 11:40:23 -0700

    Refactored home page tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,23 +4,21 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home page" do
+    before { visit root_path }
+
     it "should have the 'Super Funtime Pool Finder' button" do
-      visit root_path
       expect(page).to have_content('SUPER FUNTIME POOL FINDER')
     end
 
     it "should have the right title" do
-      visit root_path
       expect(page).to have_title("SFTPF | Home")
     end
 
     it "should have FIND POOLS button" do
-      visit root_path
       expect(page).to have_content('FIND ME SOME POOLS')
     end
 
     it "should have CREATE LOG button" do
-      visit root_path
       expect(page).to have_content('I SKATED A POOL')
     end
 
